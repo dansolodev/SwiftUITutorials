9aef1a6c0240fc7c40aee630d6595b4643e88b3a
Dan Solo <dansolo.cc@gmail.com>
2021-06-29 11:45:27 -0500

    Create the List of Landmarks.

---
+++ LandmarksApp/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  LandmarksApp
+//
+//  Created by Daniel Cinto on 29/06/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
