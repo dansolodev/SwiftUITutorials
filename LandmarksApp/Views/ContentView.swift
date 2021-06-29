4d66132fe407a377e44ce8370821e0c69c7fc603
Dan Solo <dansolo.cc@gmail.com>
2021-06-29 12:46:54 -0500

    Pass data into child views.

--- LandmarksApp/Views/ContentView.swift
+++ LandmarksApp/Views/ContentView.swift
@@ -9,44 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                
-                Text("Turtle Rock")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                
-                HStack {
-                    
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                    
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                Text("About Turtle Rock")
-                    .font(.caption)
-                Text("Descriptive text goes here.")
-                    .font(.caption2)
-                
-            }
-            .padding()
-            Spacer()
-            
-        }
+        LandmarkList()
     }
 }
 
