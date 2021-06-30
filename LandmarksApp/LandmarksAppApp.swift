aede3c5d83447563d90b1aa29e7c28c6ee76d865
Dan Solo <dansolo.cc@gmail.com>
2021-06-29 20:51:04 -0500

    Create a Favorite button for each landmark

--- LandmarksApp/LandmarksAppApp.swift
+++ LandmarksApp/LandmarksAppApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct LandmarksAppApp: App {
+    
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
--- LandmarksApp/Views/Landmarks/LandmarkDetail.swift
+++ LandmarksApp/Views/Landmarks/LandmarkDetail.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+            modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+        }
     
     var body: some View {
         
@@ -24,10 +29,14 @@ struct LandmarkDetail: View {
 
             VStack(alignment: .leading) {
                 
-                Text(landmark.name)
-                    .font(.title)
-                    .foregroundColor(.primary)
-
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                        .foregroundColor(.primary)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
+                
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -51,6 +60,6 @@ struct LandmarkDetail: View {
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[0])
     }
 }
